a41f298a1dffccbecfc28408037c895de1207c07
plaksh175_comcast <PiramanayagamMoneka_Lakshmi@comcast.com>
2025-11-24 08:31:30 +0000

    RDKEMW-10008: L1 coverage for libsyswrapper
    Reason for change: Adding L1 testcases
    
    Signed-off-by: MonekaLakshmi <monekalakshmi@gmail.com>

--- test/run_ut.sh
+++ test/run_ut.sh
@@ -9,7 +9,11 @@ export top_srcdir=`pwd`
 
 ./configure --enable-gtestapp
 
+make clean
 make -C source/test
 
 ./source/test/syswrapper_gtest.bin
 
+lcov --capture --directory . --output-file coverage.info
+lcov --remove coverage.info '/usr/*' '*gtest*' '*libsyswrapper*' --output-file coverage.info
+lcov --list coverage.info
